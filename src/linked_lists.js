271e6a1af4fa33626e464d3f9f6e7ffe5b6cac76
Zyon Z <zyonzarhaf@gmail.com>
2023-08-26 17:48:34 -0300

    Refactor all list operations

--- src/linked_lists.js
+++ src/linked_lists.js
@@ -3,9 +3,8 @@
 LINKED LISTS
 ======================
 */
-
 //Node
-class Node {
+export class Node {
   constructor(element, nextNode = undefined) {
     this.element = element;
     this.nextNode = nextNode;
@@ -13,23 +12,86 @@ class Node {
 }
 
 //List
-class LinkedList {
+export class LinkedList {
   constructor() {
     this.size = 0;
     this.head = undefined;
   }
 
-  insertFront(element) {
-    this.head = new Node(element, this.head);
+  isEmpty() {
+    return this.getSize() === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getNodeAt(index) {
+    if (!(index >= 0 && index < this.getSize())) {
+      throw new Error("Index is out of range");
+    }
+
+    let currentNode = this.head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.nextNode;
+    }
+    return currentNode;
+  }
+
+  indexOf(element) {
+    let currentNode = this.head;
+    for (let i = 0; i < this.getSize(); i++) {
+      currentNode = currentNode.nextNode;
+      if (currentNode.element === element) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+  insert(element, index) {
+    if (!(index >= 0 && index <= this.getSize())) {
+      throw new Error("Index is out of range");
+    }
+    const node = new Node(element);
+    if (index === 0) {
+      [node.nextNode, this.head] = [this.head, node];
+    } else {
+      let previousNode = this.getNodeAt(index - 1);
+      let currentNode = previousNode.nextNode;
+      [previousNode.nextNode, node.nextNode] = [node, currentNode];
+    }
     this.size++;
   }
 
-  insertBack(element) {
+  remove(element) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  removeAt(index) {
+    if (index === 0) {
+      const removed = this.head;
+      this.head = this.head.nextNode;
+      this.size--;
+      return removed.element;
+    }
+    const currentNode = this.getNodeAt(index - 1);
+    const removedNode = currentNode.next;
+    currentNode.next = removedNode.next;
+    return removedNode.element;
+  }
+
+  push(element) {
     if (!this.head) {
       this.head = new Node(element, this.head);
     } else {
       let currentNode = this.head;
-      for (let i = 0; i < this.size - 1; i++) {
+      for (let i = 0; i < this.getSize() - 1; i++) {
         currentNode = currentNode.nextNode;
       }
       currentNode.nextNode = new Node(element);
@@ -37,121 +99,16 @@ class LinkedList {
     this.size++;
   }
 
-  insertAt(element, index) {
-    if (!(index >= 0 && index <= this.size)) {
-      throw new Error('Index is out of range');
-    } else {
-      const node = new Node(element);
-      let currentNode = this.head;
-      let previousNode;
-      if (index === 0) {
-        node.nextNode = currentNode;
-        this.head = node;
-      } else {
-        for (let i = 0; i < index; i++) {
-          [previousNode, currentNode] = [currentNode, currentNode.nextNode];
-        }
-        [previousNode.nextNode, node.nextNode] = [node, currentNode];
+  toString(separator = "") {
+    if (!this.isEmpty()) {
+      let objString = `${this.head.element}`;
+      let current = this.head.nextNode;
+      for (let i = 1; i < this.getSize(); i++) {
+        objString += `${separator}${current.element}`;
+        current = current.nextNode;
       }
-      this.size++;
+      return objString;
     }
-  }
-
-  removeFirst() {
-    if (!this.head) return undefined;
-
-    const removed = this.head;
-    this.head = this.head.nextNode;
-    this.size--;
-    return removed.element;
-  }
-
-  removeLast() {
-    if (!this.head) return undefined;
-
-    if (this.size === 1) {
-      const removed = this.head;
-      this.head = undefined;
-      this.size--;
-      return removed.element;
-    }
-
-    let currentNode = this.head;
-    let previousNode;
-    for (let i = 0; i < this.size - 1; i++) {
-      [currentNode, previousNode] = [currentNode.nextNode, currentNode];
-    }
-
-    const removed = currentNode;
-    previousNode.nextNode = undefined;
-    this.size--;
-    return removed.element;
-  }
-
-  removeAt(index) {
-    if (!(index >= 0 && index <= this.size)) {
-      throw new Error('Index is out of range');
-    } else {
-      if (index === 0) {
-        const removed = this.head;
-        this.head = this.head.nextNode;
-        this.size--;
-        return removed.element;
-      } else {
-        let currentNode = this.head;
-        let previousNode;
-        for (let i = 0; i < index; i++) {
-          [previousNode, currentNode] = [currentNode, currentNode.nextNode];
-        }
-        const removed = currentNode;
-        if (index === this.size - 1) {
-          previousNode.nextNode = undefined;
-        } else {
-          previousNode.nextNode = currentNode.nextNode;
-        }
-        this.size--;
-        return removed.element;
-      }
-    }
-  }
-
-  peekBack() {
-    if (!this.head) return undefined;
-
-    let currentNode = this.head;
-    for (let i = 0; i < this.size - 1; i++) {
-      currentNode = currentNode.nextNode;
-    }
-    return currentNode.element;
-  }
-
-  peekFront() {
-    if (!this.head) return undefined;
-
-    return this.head.element;
-  }
-
-  peekAt(index) {
-    if (!this.head) return undefined;
-
-    if (!(index >= 0 && index <= this.size)) {
-      throw new Error('Index is out of range');
-    } else {
-      let currentNode = this.head;
-      for (let i = 0; i < index; i++) {
-        currentNode = currentNode.nextNode;
-      }
-      return currentNode.element;
-    }
-  }
-
-  printListData() {
-    let currentNode = this.head;
-    while (currentNode) {
-      console.log(currentNode.element);
-      currentNode = currentNode.nextNode;
-    }
+    return "";
   }
 }
-
-const ll = new LinkedList();

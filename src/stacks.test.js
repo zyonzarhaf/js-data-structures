da7a51c24311737219e4f7960260974211fd48b0
Zyon Z <zyonzarhaf@gmail.com>
2023-08-30 19:11:49 -0300

    Add remaining tests

--- src/stacks.test.js
+++ src/stacks.test.js
@@ -12,18 +12,15 @@ describe("Stack", () => {
       expect(stack.isEmpty()).toBeTruthy();
     });
 
-    it("returns false for a non-empty-string", () => {
+    it("returns false for a non-empty stack", () => {
       stack.push(1);
       expect(stack.isEmpty()).toBeFalsy();
     });
   });
 
   describe("getSize()", () => {
-    it("returns the size of the stack to equals 0", () => {
+    it("returns the number of elements in the stack", () => {
       expect(stack.getSize()).toBe(0);
-    });
-
-    it("returns the size of the stack to equals 3", () => {
       stack.push(1);
       stack.push(2);
       stack.push(3);
@@ -32,15 +29,34 @@ describe("Stack", () => {
   });
 
   describe("peek()", () => {
+    it("returns the top element of the stack", () => {
+      expect(stack.peek()).toBeUndefined();
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
+      expect(stack.peek()).toEqual(3);
+    });
+  });
+
+  describe("push(element)", () => {
+    it("adds an element to the top of the stack", () => {
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
+      expect(stack.peek()).toEqual(3);
+    });
+  });
+
+  describe("pop()", () => {
     it("returns undefined for an empty stack", () => {
-      expect(stack.peek()).toBe(undefined);
+      expect(stack.pop()).toBeUndefined();
     });
 
-    it("returns the last element from the stack to equals 3", () => {
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        expect(stack.peek()).toEqual(3);
+    it("removes the top element from the stack and returns it", () => {
+      stack.push(1);
+      stack.push(2);
+      stack.push(3);
+      expect(stack.pop()).toBe(3);
     });
   });
 });

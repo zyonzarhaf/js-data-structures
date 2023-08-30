3b99865818b84947def7df7d3693e6ea6eb12a55
Zyon Z <zyonzarhaf@gmail.com>
2023-08-30 13:33:06 -0300

    Refactor all stack operations

--- src/stacks.js
+++ src/stacks.js
@@ -3,8 +3,8 @@
 STACK DATA STRUCTURE
 ===========================
 */
-//Using objects && es6+ class syntax
-class objStack {
+// Using objects
+class Stack {
   constructor() {
     this.index = 0;
     this.items = {};
@@ -14,59 +14,55 @@ class objStack {
     return this.index === 0;
   }
 
-  size() {
+  getSize() {
     return this.index;
   }
 
-  //LIFO stuff
-  peekBack() {
+  peek() {
     if (!this.isEmpty()) {
       return this.items[this.index - 1];
-    } else {
-      return undefined;
     }
+    return undefined;
   }
 
-  addBack(element) {
+  push(element) {
     this.items[this.index] = element;
     this.index++;
   }
 
-  removeBack() {
+  pop() {
     if (!this.isEmpty()) {
       const removed = this.items[this.index - 1];
       delete this.items[this.index - 1];
       this.index--;
       return removed;
-    } else {
-      return undefined;
     }
+    return undefined;
   }
 
-  //utils
   toString() {
-    if (!this.isEmpty()) {
+    if (!this.isEmpty(separator)) {
       let stackString = `${this.items[0]}`;
       for (let i = 1; i < this.index; i++) {
-        stackString += `,${this.items[i]}`;
+        stackString += `${separator}${this.items[i]}`;
       }
       return stackString;
-    } else {
-      return undefined;
     }
+    return undefined;
   }
 
   reverse() {
     let firstIndex = 0;
-    for (let i = 0; i < this.index; i++) {
-      [this.items[firstIndex], this.items[this.index - 1]] = [
-        this.items[this.index - 1],
-        this.items[firstIndex],
-      ];
+    let lastIndex = this.index - 1;
+    for (let i = 0; i < lastIndex; i++) {
+      [this.items[firstIndex], 
+      this.items[lastIndex]]
+      = 
+      [this.items[lastIndex],
+      this.items[firstIndex]];
       firstIndex++;
-      this.index--; //will mess w/ index value
+      lastIndex--;
     }
-    this.index = firstIndex + this.index; //restore index value
   }
 
   clear() {
@@ -75,154 +71,50 @@ class objStack {
   }
 }
 
-//Dec to binary w/ this approach
+// Dec to binary
 const decToBinary = function (value) {
   let dec = value;
   let rem;
+  const stack1 = new Stack();
+  const stack2 = new Stack();
 
-  const remStack = new objStack();
   while (dec > 0) {
     rem = Math.floor(dec % 2);
     dec = Math.floor(dec / 2);
-    remStack.addBack(rem);
+    stack1.push(rem);
   }
 
-  remStack.reverse();
+  while (!stack1.isEmpty()) {
+    stack2.push(stack1.pop());
+  }
 
-  const binaryString = remStack.toString();
-
-  console.log(binaryString);
-
-  return binaryString;
+  return stack2.toString();
 };
 
-decToBinary(324);
-
-//Dec to anything from base 2 to 36
-const baseConverter = function (val, base) {
+// Dec to anything from base 2 to 36
+const baseConverter = function (value, base) {
   if (!(base >= 2 && base <= 36)) {
-    return 'must be a base value from 2 to 36';
-  } else {
-    const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let value = val;
-    let rem;
-
-    //fill and reverse the stack
-    const baseStack = new objStack();
-    while (value > 0) {
-      rem = Math.floor(value % base);
-      value = Math.floor(value / base);
-      baseStack.addBack(rem);
-    }
-    baseStack.reverse();
-
-    //get base string
-    let baseString = `${digits[baseStack.items[0]]}`;
-    for (let i = 1; i < baseStack.index; i++) {
-      baseString += `,${digits[baseStack.items[i]]}`;
-    }
-
-    console.log(baseString);
-
-    return baseString;
+    return "must be a base value from 2 to 36";
   }
-};
-
-baseConverter(15790288, 16); //F0F0D0 hex num
-
-//Using arrays
-class arrStack {
-  constructor() {
-    this.items = [];
-  }
-
-  isEmpty() {
-    return this.items.length === 0;
-  }
-
-  size() {
-    return this.items.length;
-  }
-
-  //LIFO stuff
-  peekBack() {
-    if (!this.isEmpty()) {
-      return this.items[this.items.length - 1];
-    } else {
-      return undefined;
-    }
-  }
-
-  addBack(element) {
-    this.items.push(element);
-  }
-
-  removeBack() {
-    return this.items.pop();
-  }
-
-  //utils
-  toString() {
-    return this.items.toString();
-  }
-
-  reverse() {
-    this.items.reverse();
-  }
-
-  clear() {
-    this.items = [];
-  }
-}
-
-//Dec to binary w/ this approach
-const arrDecToBinary = function (value) {
-  let dec = value;
+  const digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let rem;
+  let val = value;
+  const stack1 = new Stack();
+  const stack2 = new Stack();
 
-  const remStack = new arrStack();
-  while (dec > 0) {
-    rem = Math.floor(dec % 2);
-    dec = Math.floor(dec / 2);
-    remStack.addBack(rem);
+  while (value > 0) {
+    rem = Math.floor(val % base);
+    val = Math.floor(val / base);
+    stack1.push(rem);
   }
-  remStack.reverse();
 
-  const binaryString = remStack.toString();
-
-  console.log(binaryString);
-
-  return binaryString;
-};
-
-arrDecToBinary(431);
-
-//Dec to anything from base 2 to 36
-const arrBaseConverter = function (val, base) {
-  if (!(base >= 2 && base <= 36)) {
-    return 'must be a base value from 2 to 36';
-  } else {
-    const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    let value = val;
-    let rem;
-
-    const baseStack = new arrStack();
-    while (value > 0) {
-      rem = Math.floor(value % base);
-      value = Math.floor(value / base);
-      baseStack.addBack(rem);
-    }
-    baseStack.reverse();
-
-    let baseString = `${digits[baseStack.items[0]]}`;
-    for (let i = 1; i < baseStack.size(); i++) {
-      baseString += `,${digits[baseStack.items[i]]}`;
-    }
-
-    console.log(baseString);
-
-    return baseString;
+  while(!stack1.isEmpty()) {
+    stack2.push(stack1.pop());
   }
-};
-
-arrBaseConverter(25100328, 16);
+  
+  let baseString = `${digits[stack2.items[0]]}`;
+  for (let i = 1; i < stack2.index; i++) {
+    baseString += `,${digits[stack2.items[i]]}`;
+  }
+  return baseString;
+};  
